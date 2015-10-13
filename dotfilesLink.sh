a0173ead94b2609acbe27f458c808e58852e09c7
Ohara Naoya <ohara_naoya@kakaku.com>
2015-10-13 17:38:25 +0900

    Change git ignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
